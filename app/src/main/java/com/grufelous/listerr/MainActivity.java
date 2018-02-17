8eb55d517a6cbc633eac0e2e9f05cee9f8c1f928
grufelous <grufelous@gmail.com>
2018-02-18 00:04:20 +0530

    Added adapter class for Contact
    ContactAdapter and Contact created along with required constructors

--- app/src/main/java/com/grufelous/listerr/MainActivity.java
+++ app/src/main/java/com/grufelous/listerr/MainActivity.java
@@ -1,7 +1,13 @@
 package com.grufelous.listerr;
 
+import android.content.Context;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,6 +20,22 @@ public class MainActivity extends AppCompatActivity {
     public class Contact {
         String leadingText, addressText, aboutText;
         int imageID;
+        public Contact(String leadingText, String addressText, String aboutText, int imageID) {
+            this.leadingText = leadingText;
+            this.addressText = addressText;
+            this.aboutText = aboutText;
+            this.imageID = imageID;
+        }
+    }
 
+    public class ContactAdapter extends ArrayAdapter<Contact> {
+        private ArrayList<Contact> list;
+        private int layoutResource;
+        @Override
+        public ContactAdapter(@NonNull Context context, int resource, @NonNull List<Contact> objects) {
+            super(context, resource, objects);
+            layoutResource = resource;
+            list = (ArrayList<Contact>) objects;
+        }
     }
 }
