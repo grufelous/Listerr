20c1a3e69d3471c47bce79ea62c7e9697cc051fe
grufelous <grufelous@gmail.com>
2018-02-21 22:13:57 +0530

    Bottom navigator, updated theme, modified menu

--- app/src/main/java/com/grufelous/listerr/MainActivity.java
+++ app/src/main/java/com/grufelous/listerr/MainActivity.java
@@ -3,12 +3,18 @@ package com.grufelous.listerr;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.internal.BottomNavigationMenu;
+import android.support.design.internal.BottomNavigationMenuView;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,10 +23,20 @@ import de.hdodenhof.circleimageview.CircleImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ListView contactListView;
+    private BottomNavigationView bottomNavigationMenu;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        contactListView = findViewById(R.id.listy);
+        bottomNavigationMenu = findViewById(R.id.bottom_navigation);
+        Toast t = Toast.makeText(getApplicationContext(), "Selected item: " + bottomNavigationMenu.getSelectedItemId(), Toast.LENGTH_LONG);
+        t.setGravity(Gravity.LEFT, 20, -15);
+        t.show();
+        ArrayList<Contact> contactArrayList = new ArrayList<>();
+        ContactAdapter contactAdapter = new ContactAdapter(this, R.layout.contact_layout, contactArrayList);
+        contactListView.setAdapter(contactAdapter);
     }
 
     public class Contact {
